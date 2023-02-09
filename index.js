315dd11337f787c720b447b4d8fe5bcbf37bac49
SoftPoachedEggs <115499632+SoftPoachedEggs@users.noreply.github.com>
2023-02-09 13:11:24 -0500

    Add files via upload
    updated license section and bringing version up to current

--- index.js
+++ index.js
@@ -1,49 +1,77 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs');
+
+
+const licensesObj = ['APACHE','BSD','GPL', 'MIT', 'None' ]
 
 // TODO: Create an array of questions for user input
 const questions = [
-inquirer
-  .prompt([
+inquirer.prompt([
     {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is the name of project',
+        name: 'projectname',
       },
       {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is a description of the project?',
+        name: 'projectdescription',
       },
       {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What are the insallation instructions?',
+        name: 'installinstructions',
       },
       {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What is your projects inteded use?',
+        name: 'intendeduse',
       },
       {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
+        message: 'What are your projects collaborators or 3rd party assets?',
+        name: 'collaborators',
+      },
+      {
+        type: 'list',
+        message: 'What is your project license?',
+        choices: licensesObj,
+        name: 'license'
       },
       {
         type: 'input',
-        message: 'What is your user name?',
-        name: 'username',
-      },      
+        message: 'What is your github username?',
+        name: 'gitusername',
+      },
+      {
+        type: 'input',
+        message: 'What is your contact email?',
+        name: 'emailcontact',
+      },    
     ])
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const readme = "./readmefolder/readme.md";
+
+  fs.writeFile("output.txt", "Hello World!", function(err) {
+    if(err) {
+        return console.error(err);
+    }
+    console.log("File created successfully!");
+  });
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.promt(questions)
+  .then (answers => writeToFile(generateMarkdown(answers)))
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,24 +2,61 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   let badge = "";
-  if (license != null ) {
-    badge = "![License Badge](https://shields.io/badge/license-" + license + "-green)";
+  if (license != 'None' ) {
+    badge = "![License Badge](https://img.shields.io/badge/license-" + license + "-orange);"
   }
 }
   
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = ""; 
+
+    //switch statement to check if selected license. If so, save the associated link to licenseLink var.
+    switch(license) {
+    case "APACHE": 
+      licenseLink = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+    break; 
+    case "BSD": 
+      licenseLink = "https://opensource.org/licenses/BSD-3-Clause"
+    break;
+    case "GPL": 
+      licenseLink = "https://www.gnu.org/licenses/gpl-3.0.txt"
+    break;
+    case "MIT": 
+      licenseLink = "https://opensource.org/licenses/MIT"
+    break;
+    default:
+      licenseLink = ""; 
+
+  }
+}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+let licenseSection = ""; 
+
+if (license != 'None') {
+  markdown += '## License\n'
+  markdown += 'Visit ' + renderLicenseLink(license) + ' for more information'
+}
+
+}
+//name of project projectname
+//description projectdescription
+//install instructions installinstructions
+//intended use intendeduse
+//collaborators/3p collaborators
+//licenses license
+//git Username gitusername
+//email emailcontact
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  return `# ${data.title}
 }
 
 module.exports = generateMarkdown;
